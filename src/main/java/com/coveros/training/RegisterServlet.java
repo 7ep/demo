57368d101af7a78e44c205c0a0be97e4bddf82b3
Byron Katz <byronka@msn.com>
2018-09-20 17:23:03 -0400

    formatting and adding code to handle situation if database doesn't exist

--- src/main/java/com/coveros/training/RegisterServlet.java
+++ src/main/java/com/coveros/training/RegisterServlet.java
@@ -20,14 +20,14 @@ import java.util.StringTokenizer;
 
 import java.nio.charset.StandardCharsets;
 
-@WebServlet(name = "RegisterServlet", urlPatterns = {"register"}, loadOnStartup = 1) 
+@WebServlet(name = "RegisterServlet", urlPatterns = {"register"}, loadOnStartup = 1)
 public class RegisterServlet extends HttpServlet {
 
-  private static String DATABASE_NAME = "database.txt";
+    private static String DATABASE_NAME = "database.txt";
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        response.getWriter().print("Hello, World!");  
+            throws ServletException, IOException {
+        response.getWriter().print("Hello, World!");
     }
 
     private String putUsernameInRequest(HttpServletRequest request) {
@@ -45,61 +45,65 @@ public class RegisterServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         String username = putUsernameInRequest(request);
         String password = putPasswordInRequest(request);
 
         if (!isUserInDatabase(username)) {
-          saveToDatabase(username, password);
-          request.getRequestDispatcher("registration.jsp").forward(request, response); 
+            saveToDatabase(username, password);
+            request.getRequestDispatcher("registration.jsp").forward(request, response);
         } else {
-          request.getRequestDispatcher("already_registered.jsp").forward(request, response); 
+            request.getRequestDispatcher("already_registered.jsp").forward(request, response);
         }
     }
 
-   protected void saveToDatabase(String username, String password) {
-     saveTextToFile(username + " " + password);
-   }
+    protected void saveToDatabase(String username, String password) {
+        saveTextToFile(username + " " + password);
+    }
 
-   protected boolean isUserInDatabase(String username) {
-     try {
-      File database = new File(DATABASE_NAME);
-      try (BufferedReader br = new BufferedReader(new FileReader(database))) {
-        String line;
-        while ((line = br.readLine()) != null) {
-          StringTokenizer st = new StringTokenizer(line);
-          while (st.hasMoreTokens()) {
-            if (st.nextToken().equals(username)) {
-              return true;
+    protected boolean isUserInDatabase(String username) {
+        try {
+            File database = new File(DATABASE_NAME);
+            if (!database.exists() || database.isDirectory()) {
+                return false;
             }
-          }
-        }
-        // if we get to this point, we never found that username
-        return false;
-      }
-     } catch (Exception ex) {
-       throw new RuntimeException(ex);
-     }
-   }
+            try (BufferedReader br = new BufferedReader(new FileReader(database))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    StringTokenizer st = new StringTokenizer(line);
+                    while (st.hasMoreTokens()) {
+                        if (st.nextToken().equals(username)) {
+                            return true;
+                        }
+                    }
+                }
+                // if we get to this point, we never found that username
+                return false;
+            }
 
-   /**
-    * save text to a file.  If the file exists, append.  If 
-    * the file doesn't exist, create a new file.
-    */
-   private void saveTextToFile(String text) {
-      try {
-          final Path path = Paths.get(DATABASE_NAME);
-          StandardOpenOption openOption = Files.exists(path) ? 
-                       StandardOpenOption.APPEND : 
-                       StandardOpenOption.CREATE;
-          Files.write(path, 
-                      Arrays.asList(text), 
-                      StandardCharsets.UTF_8,
-                      openOption );
-      } catch (final IOException ioe) {
-          throw new RuntimeException(ioe);
-      }
-   }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * save text to a file.  If the file exists, append.  If
+     * the file doesn't exist, create a new file.
+     */
+    private void saveTextToFile(String text) {
+        try {
+            final Path path = Paths.get(DATABASE_NAME);
+            StandardOpenOption openOption = Files.exists(path) ?
+                    StandardOpenOption.APPEND :
+                    StandardOpenOption.CREATE;
+            Files.write(path,
+                    Arrays.asList(text),
+                    StandardCharsets.UTF_8,
+                    openOption );
+        } catch (final IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
 
 }
 
