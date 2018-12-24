74f0d67237348e677a4a33b56e23b49d1e83c41d
Byron Katz <byronka@msn.com>
2018-12-24 13:25:15 -0500

    Splitting unit and integration tests into their own directories.

--- src/integration_test/java/com/coveros/training/BookCheckOutStepDefs.java
+++ src/integration_test/java/com/coveros/training/BookCheckOutStepDefs.java
@@ -1,5 +1,6 @@
 package com.coveros.training;
 
+import com.coveros.training.*;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
--- src/integration_test/java/com/coveros/training/LoginStepDefs.java
+++ src/integration_test/java/com/coveros/training/LoginStepDefs.java
@@ -1,5 +1,8 @@
 package com.coveros.training;
 
+import com.coveros.training.DatabaseUtils;
+import com.coveros.training.LoginUtils;
+import com.coveros.training.RegistrationUtils;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
