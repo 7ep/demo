b9f5b22ad4950a3fa77ef1b6a61c4ce0856cfeff
Byron Katz <byronka@msn.com>
2018-12-24 15:24:36 -0500

    Adjusting to use environment and property values.

--- src/main/java/com/coveros/training/Constants.java
+++ src/main/java/com/coveros/training/Constants.java
@@ -1,7 +1,35 @@
 package com.coveros.training;
 
+/**
+ * These values are important and dependent on correctly set
+ * environment variables.
+ */
 class Constants {
-    static final String RESTORE_SCRIPTS_PATH = "C:\\Users\\byron\\demo\\db_sample_files\\";
-    static final String PATH_TO_PG_RESTORE = "C:\\Program Files\\PostgreSQL\\10\\bin\\pg_restore.exe";
-    static final String DATABASE_URL = "jdbc:postgresql://localhost/training";
+
+    /**
+     * This is a wrapper about System.getenv, except that we will
+     * throw an exception if we don't get a value.
+     * @param name the name of the environment variable
+     * @return a non-empty String, or else we throw an exception
+     */
+    private static String getEnvironmentVariable(String name) {
+        final String value = StringUtils.makeNotNullable(System.getenv(name));
+        if (value.isEmpty()) {
+            throw new RuntimeException("environment variable returned an empty string.");
+        }
+        return value;
+    }
+
+    // DEMO_PROJECT_HOME is the absolute path to the demo project.
+    // For example, C:\Users\byron\demo\
+    static final String RESTORE_SCRIPTS_PATH = getEnvironmentVariable("DEMO_PROJECT_HOME") + "\\db_sample_files\\";
+
+    // POSTGRES_BIN_DIR must be set to the directory for Postgresql's binaries.
+    // for example, on Windows it's C:\Program Files\PostgreSQL\10\bin\
+    static final String PATH_TO_PG_RESTORE = getEnvironmentVariable("POSTGRES_BIN_DIR") + "\\pg_restore.exe";
+
+    // we'll default to the following URL for our Poostgresql database, unless
+    // it is set in the environment differently.
+    private static final String DEFAULT_DB_URL = "jdbc:postgresql://localhost/training";
+    static final String DATABASE_URL = System.getProperty("POSTGRES_URL", DEFAULT_DB_URL);
 }
