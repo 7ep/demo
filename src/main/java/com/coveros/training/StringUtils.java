b7cb9a732986aa3c3d83290a13552c629adc33e5
Byron Katz <byronka@msn.com>
2019-01-11 15:12:55 -0500

    Adding a private constructor to hide the implicit public one.

--- src/main/java/com/coveros/training/StringUtils.java
+++ src/main/java/com/coveros/training/StringUtils.java
@@ -7,6 +7,10 @@ import org.checkerframework.checker.nullness.qual.Nullable;
  */
 public class StringUtils {
 
+    private StringUtils() {
+        // using a private constructor to hide the implicit public one.
+    }
+
     /**
      * checks the String you pass in; if it's null, return an empty String.
      * Otherwise, return the unchanged string.
