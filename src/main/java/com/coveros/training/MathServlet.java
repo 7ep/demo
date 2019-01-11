0e52cad943666c892be9ef0c5e05438293c739c4
Byron Katz <byronka@msn.com>
2019-01-10 21:11:17 -0600

    Automatic reformatting, indenting.

--- src/main/java/com/coveros/training/MathServlet.java
+++ src/main/java/com/coveros/training/MathServlet.java
@@ -13,30 +13,30 @@ public class MathServlet extends HttpServlet {
 
   private static final Logger logger = LogManager.getLogger();
 
-    private int putNumberInRequest(String itemName, HttpServletRequest request) {
-    
-        int item;
-        try {
-          item = Integer.parseInt(request.getParameter(itemName));
-          request.setAttribute(itemName, item);
-        } catch (NumberFormatException ex) {
-          item = 0;
-        }
-        return item;
+  private int putNumberInRequest(String itemName, HttpServletRequest request) {
+
+    int item;
+    try {
+      item = Integer.parseInt(request.getParameter(itemName));
+      request.setAttribute(itemName, item);
+    } catch (NumberFormatException ex) {
+      item = 0;
     }
+    return item;
+  }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
-        int item_a = putNumberInRequest("item_a", request);
-        int item_b = putNumberInRequest("item_b", request);
+    int item_a = putNumberInRequest("item_a", request);
+    int item_b = putNumberInRequest("item_b", request);
 
-      setResultToSum(request, item_a, item_b);
-      try {
-        request.getRequestDispatcher("result.jsp").forward(request, response);
-      } catch (Exception ex) {
-        logger.error("failed during forward: " + ex);
-      }
+    setResultToSum(request, item_a, item_b);
+    try {
+      request.getRequestDispatcher("result.jsp").forward(request, response);
+    } catch (Exception ex) {
+      logger.error("failed during forward: " + ex);
     }
+  }
 
   void setResultToSum(HttpServletRequest request, int item_a, int item_b) {
     request.setAttribute("result", doAdd(item_a, item_b));
@@ -44,6 +44,6 @@ public class MathServlet extends HttpServlet {
 
 
   private int doAdd(int a, int b) {
-     return a + b;
-   }
+    return a + b;
+  }
 }
