04354267a69f0dcbe22f70e9b18d9f59b04e2690
Byron Katz <byronka@msn.com>
2019-01-10 20:23:22 -0600

    adjusting indent and making less broadly scoped.

--- src/main/java/com/coveros/training/Constants.java
+++ src/main/java/com/coveros/training/Constants.java
@@ -6,10 +6,10 @@ import java.nio.file.Paths;
  * These values are important and dependent on correctly set
  * environment variables.
  */
-public class Constants {
+class Constants {
 
-    // we'll default to the following URL for our Poostgresql database, unless
-    // it is set in the environment differently.
-    private static final String DEFAULT_DB_URL = "jdbc:postgresql://localhost/training";
-    public static final String DATABASE_URL = System.getProperty("POSTGRES_URL", DEFAULT_DB_URL);
+  // we'll default to the following URL for our Poostgresql database, unless
+  // it is set in the environment differently.
+  private static final String DEFAULT_DB_URL = "jdbc:postgresql://localhost/training";
+  static final String DATABASE_URL = System.getProperty("POSTGRES_URL", DEFAULT_DB_URL);
 }
