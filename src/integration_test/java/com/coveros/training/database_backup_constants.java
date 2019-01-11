743bf02c63b6f0b119d7ea6724db7071aba5c8ef
Byron Katz <byronka@msn.com>
2019-01-10 20:37:27 -0600

    Removing unused constants.

--- src/integration_test/java/com/coveros/training/database_backup_constants.java
+++ src/integration_test/java/com/coveros/training/database_backup_constants.java
@@ -2,16 +2,6 @@ package com.coveros.training;
 
 public class database_backup_constants {
 
-    /**
-     * This one is just an empty version 1 database
-     */
-    public static final String SAMPLE_DB_V1_DUMP = "sample_db_v1.dump";
-
-    /**
-     * This one is the version 1 database with a single entry in the persons table
-     */
-    public static final String ONE_PERSON_IN_TABLE_ALREADY_V1_DUMP = "one_person_in_table_already_v1.dump";
-
     public static final String INITIAL_STATE_V2_DUMP = "initial_empty_state_v2.dump";
 
     public static final String ONE_PERSON_IN_BORROWER_TABLE_V2_DUMP = "one_person_in_borrower_table_v2.dump";
