46f4b64d8f73f29cd34c1c245080e922022e7bb4
Byron Katz <byronka@msn.com>
2019-01-15 23:29:51 -0500

    Removing an unused import.

--- src/main/java/com/coveros/training/RegisterServlet.java
+++ src/main/java/com/coveros/training/RegisterServlet.java
@@ -1,7 +1,6 @@
 package com.coveros.training;
 
 import com.coveros.training.domainobjects.RegistrationResult;
-import com.coveros.training.persistence.PersistenceLayer;
 import com.coveros.training.persistence.RegistrationUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
