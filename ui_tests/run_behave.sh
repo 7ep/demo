974ee22b8405d5c2d98fa641f0198389359506e7
Byron Katz <byronka@msn.com>
2019-01-21 10:47:49 -0500

    continuing to adjust to use pipenv

--- ui_tests/run_behave.sh
+++ ui_tests/run_behave.sh
@@ -2,4 +2,4 @@
 
 cd ~/demo_tests/ui_tests
 PATH=/usr/lib64/qt-3.3/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/brk/.local/bin:/home/brk/bin
-DISPLAY=:0 behave
+DISPLAY=:0 pipenv run behave
