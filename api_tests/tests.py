b6854bf2e7eeb50d41e766cb4574f4ae94d448d3
Byron Katz <byronka@msn.com>
2019-01-30 13:27:04 -0500

    Refactoring - removing duplication in URL's.

--- api_tests/tests.py
+++ api_tests/tests.py
@@ -3,6 +3,9 @@ import requests
 import subprocess
 import os
 
+SERVER = "localhost"
+URL = "http://%s:8080" % SERVER
+
 
 class TestStringMethods(unittest.TestCase):
 
@@ -10,7 +13,8 @@ class TestStringMethods(unittest.TestCase):
     def __reset_database():
         cmd = ['pg_restore',
                '--username=postgres',
-               '--dbname', 'training',
+               ('--host=%s' % SERVER),
+               '--dbname=training',
                '--no-password',
                '--clean', '../db_sample_files/initial_empty_state_v2.dump']
         FNULL = open(os.devnull, 'w')
@@ -18,20 +22,20 @@ class TestStringMethods(unittest.TestCase):
         FNULL.close()
 
     def test_math_api(self):
-        r = requests.post("http://localhost:8080/demo/math", data = {'item_a':'9', 'item_b':'7'})
+        r = requests.post("%s/demo/math" % URL, data = {'item_a': '9', 'item_b': '7'})
         self.assertTrue("Result: 16" in r.text)
 
     # register with a good password and an empty database
     def test_register_api(self):
         self.__reset_database()
-        r = requests.post("http://localhost:8080/demo/register", data = {'username':'alice', 'password':'B65S3xNW8vXQHyjYnD72L3mejc'})
+        r = requests.post("%s/demo/register" % URL, data = {'username': 'alice', 'password': 'B65S3xNW8vXQHyjYnD72L3mejc'})
         self.assertTrue("wasSuccessfullyRegistered=true,status=SUCCESSFULLY_REGISTERED" in r.text)
 
     # register, then login
     def test_login_api(self):
         self.__reset_database()
-        r = requests.post("http://localhost:8080/demo/register", data = {'username':'alice', 'password':'B65S3xNW8vXQHyjYnD72L3mejc'})
-        r = requests.post("http://localhost:8080/demo/login", data = {'username':'alice', 'password':'B65S3xNW8vXQHyjYnD72L3mejc'})
+        r = requests.post("%s/demo/register" % URL, data = {'username': 'alice', 'password': 'B65S3xNW8vXQHyjYnD72L3mejc'})
+        r = requests.post("%s/demo/login" % URL, data = {'username': 'alice', 'password': 'B65S3xNW8vXQHyjYnD72L3mejc'})
         self.assertTrue("Result: access granted" in r.text)
 
 
