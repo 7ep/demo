e81931a4e7e0f489c261737fdf6e5bf86b5ce2ab
Byron Katz <byronka@msn.com>
2019-02-01 13:03:14 -0500

    Adjusting to use h2 db in home directory

--- src/main/java/com/coveros/training/persistence/PersistenceLayer.java
+++ src/main/java/com/coveros/training/persistence/PersistenceLayer.java
@@ -55,7 +55,7 @@ public class PersistenceLayer {
         final org.h2.jdbcx.JdbcDataSource ds = new org.h2.jdbcx.JdbcDataSource();
         ds.setUser("sa");
         ds.setPassword("sa");
-        ds.setUrl("jdbc:h2:tcp://localhost/./training;AUTO_SERVER=TRUE;MODE=PostgreSQL");
+        ds.setUrl("jdbc:h2:~/training;MODE=PostgreSQL;AUTO_SERVER=TRUE");
         return ds;
     }
 
