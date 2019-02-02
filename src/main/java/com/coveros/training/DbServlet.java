0ded33dbac4e6fb42765f63f3420eeeecc387baf
Byron Katz <byronka@msn.com>
2019-02-01 19:35:49 -0500

    Simplifying clean process.

--- src/main/java/com/coveros/training/DbServlet.java
+++ src/main/java/com/coveros/training/DbServlet.java
@@ -10,28 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "DbServlet", urlPatterns = {"/dbutils"}, loadOnStartup = 1)
+@WebServlet(name = "DbServlet", urlPatterns = {"/dbclean"}, loadOnStartup = 1)
 public class DbServlet extends HttpServlet {
 
   static Logger logger = LogManager.getLogger();
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-    final String choice = request.getParameter("choice");
-    final PersistenceLayer persistenceLayer = new PersistenceLayer();
-    if (choice.equals("clean")) {
-      Flyway flyway = Flyway.configure()
-          .schemas("ADMINISTRATIVE", "LIBRARY", "AUTH")
-          .dataSource("jdbc:h2:~/training", "sa", "sa")
-          .load();
-      flyway.clean();
-      request.setAttribute("result", "schemas dropped");
-    }
-    else if (choice.equals("restore")) {
-      persistenceLayer.runRestore("db_sample_files/v2_empty_schema.sql");
-      request.setAttribute("result", "database restored");
-    }
-
+    Flyway flyway = Flyway.configure()
+        .schemas("ADMINISTRATIVE", "LIBRARY", "AUTH")
+        .dataSource("jdbc:h2:~/training", "sa", "sa")
+        .load();
+    flyway.clean();
+    flyway.migrate();
+    request.setAttribute("result", "cleaned and migrated");
     ServletUtils.forwardToResult(request, response, logger);
   }
 
