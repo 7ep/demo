dacb843d6aa8b004e95f9ac659cd01c1e23a0052
Byron Katz <byronka@msn.com>
2019-02-07 09:39:39 -0500

    Removing unused imports.

--- src/main/java/com/coveros/training/persistence/PersistenceLayer.java
+++ src/main/java/com/coveros/training/persistence/PersistenceLayer.java
@@ -6,8 +6,6 @@ import com.coveros.training.domainobjects.Book;
 import com.coveros.training.domainobjects.Borrower;
 import com.coveros.training.domainobjects.Loan;
 import com.coveros.training.domainobjects.User;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.flywaydb.core.Flyway;
 import org.h2.jdbcx.JdbcConnectionPool;
 
