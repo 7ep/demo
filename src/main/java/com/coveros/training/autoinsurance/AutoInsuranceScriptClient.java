8a1e2f178391ccc9882b3208d7c54cfbd5f95b62
Byron Katz <byronka@msn.com>
2019-09-06 17:50:01 -0400

    Adding templates for desktop UI and client/server code.
    I am using some code I found on Oracle's Java website to
    show a Desktop UI and to provide a simple socket client and server.
    
    The intention here is to move towards an ability to make control
    of our desktop application possible through a socket.
    
    I will want to simplify the UI and let it provide access to the
    auto insurance processor.
    
    I will want to add simple abilities to script automation of that
    UI through sockets.
    
    Simplicity is key.  I don't know ahead of time how successful this
    will be.  We'll see.
    
    As an aside, the current technique to run these items (the UI, the
    client and the server) is: they all have main() methods, so you can
    run them through the UI, or..
    
    you can assemble the system:
    
    gradlew assemble
    
    and then go to the build/classes/java/main directory and run these
    commands:
    
    byron@DESKTOP-4GITO6A c:\Users\byron\demo\build\classes\java\main
    $ java com.coveros.training.autoinsurance.AutoInsuranceUI
    
    byron@DESKTOP-4GITO6A c:\Users\byron\demo\build\classes\java\main
    $ java com.coveros.training.autoinsurance.AutoInsuranceScriptServer 8000
    
    $ java com.coveros.training.autoinsurance.AutoInsuranceScriptClient localhost 8000
    hello
    echo: hello
    hi
    echo: hi
    hi
    echo: hi
    
    The future design will have the UI started through gradle, and to
    start the server whenever the UI is started.  The client will be
    assimilated into some kind of program that the testers can work with
    to automate testing of the application.

---
+++ src/main/java/com/coveros/training/autoinsurance/AutoInsuranceScriptClient.java
@@ -0,0 +1,47 @@
+package com.coveros.training.autoinsurance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class AutoInsuranceScriptClient {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            System.err.println(
+                    "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+                Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out =
+                        new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in =
+                        new BufferedReader(
+                                new InputStreamReader(echoSocket.getInputStream()));
+                BufferedReader stdIn =
+                        new BufferedReader(
+                                new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+    }
+}
---
+++ src/main/java/com/coveros/training/autoinsurance/AutoInsuranceScriptServer.java
@@ -0,0 +1,41 @@
+package com.coveros.training.autoinsurance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class AutoInsuranceScriptServer {
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try (
+                ServerSocket serverSocket =
+                        new ServerSocket(Integer.parseInt(args[0]));
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out =
+                        new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+
+            }
+        } catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
+        }
+    }
+}
