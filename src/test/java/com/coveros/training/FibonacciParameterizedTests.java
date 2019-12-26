bb783dfbd0bff868fb1d338530b6b7083cba4a0a
Byron Katz <byronka@msn.com>
2019-12-26 13:31:25 -0500

    Tidying up

--- src/test/java/com/coveros/training/FibonacciParameterizedTests.java
+++ src/test/java/com/coveros/training/FibonacciParameterizedTests.java
@@ -15,8 +15,8 @@ public class FibonacciParameterizedTests {
     @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, 5 }, { 6, 8 },
-                {20, 6765}
+                { 0, 0 }, { 1, 1 }, { 2, 1 }, { 3, 2 },
+                { 4, 3 }, { 5, 5 }, { 6, 8 }, {20, 6765}
         });
     }
 
