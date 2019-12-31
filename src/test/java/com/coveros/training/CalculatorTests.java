786dfaf9224fc41a28de4ce39b10dfec7dd707c3
Byron Katz <byronka@msn.com>
2019-12-31 16:05:39 -0500

    Adding an arbitrary bit of code to get tested.

---
+++ src/test/java/com/coveros/training/CalculatorTests.java
@@ -0,0 +1,27 @@
+package com.coveros.training;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CalculatorTests {
+
+    @Ignore("this test is for classes only")
+    @Test
+    public void testShouldAddTwoIntegers() {
+    }
+
+    @Ignore("this test is for classes only")
+    @Test
+    public void testShouldAddTwoDecimals() {
+    }
+
+    @Ignore("this test is for classes only")
+    @Test
+    public void testShouldGetStringVersionOfResult() {
+    }
+
+    @Ignore("this test is for classes only")
+    @Test
+    public void testShouldGetVectorResult() {
+    }
+}
