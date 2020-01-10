0b87f4b1f51011a9baff341143adba52d1d755cd
Byron Katz <byronka@msn.com>
2020-01-10 17:28:19 -0500

    Renaming autoinsurance to desktop_app.  Adding to ci/cd instructions.

--- src/test/java/com/coveros/training/CalculatorTests.java
+++ src/test/java/com/coveros/training/CalculatorTests.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 
 public class CalculatorTests {
 
-    @Ignore("this test is for classes only")
+    @Ignore("this test is for teaching")
     @Test
     public void testShouldAddTwoIntegers() {
     }
 
-    @Ignore("this test is for classes only")
+    @Ignore("this test is for teaching")
     @Test
     public void testShouldAddTwoDecimals() {
     }
 
-    @Ignore("this test is for classes only")
+    @Ignore("this test is for teaching")
     @Test
     public void testShouldGetStringVersionOfResult() {
     }
 
-    @Ignore("this test is for classes only")
+    @Ignore("this test is for teaching")
     @Test
     public void testShouldGetVectorResult() {
     }
