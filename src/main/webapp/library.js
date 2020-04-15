a46d6395a49236716ce8e8d99132725cc335ac04
Byron Katz <byronka@msn.com>
2020-04-14 21:41:37 -0400

    comments, refactoring, new features - dropdown.
    Now, if there are only a few items to show, shows as a dropdown.
    If there are no items in the database, locks the field.

--- src/main/webapp/library.js
+++ src/main/webapp/library.js
@@ -1,18 +1,34 @@
-function createItemForList(text, input_id) {
+/**
+  * Creates a list item. see https://www.w3.org/TR/REC-html40/struct/lists.html#edef-LI
+  * Adds an attribute, "data", that holds the text data, and also
+  * puts the text in as the inner text node
+  */
+function createItemForList(text) {
     let item = document.createElement('li');
     item.setAttribute("data",text);
     item.appendChild(document.createTextNode(text));
-
-    // if the user clicks, put the value in the input field
-    item.addEventListener('click', function(event) {
-      let lend_book = document.getElementById(input_id);
-      lend_book.value = event.target.innerText;
-      deleteSearchBox(input_id);
-    });
-
     return item;
 }
 
+/**
+  * Adds the behavior that takes place when a user clicks on 
+  * an item in the autocomplete search box.  That behavior
+  * is to put the item in the text input field
+  * @param item an item in the autocomplete list, like a book for example
+  * @param the base id on which to select parts of the searchbox
+  */
+function addItemClickBehavior(item, input_id) {
+    // if the user clicks, put the value in the input field
+    item.addEventListener('click', function(event) {
+      let input_field = document.getElementById(input_id);
+      input_field.value = event.target.innerText;
+      deleteSearchBox(input_id);
+    });
+}
+
+/**
+  * given an id, find and remove a searchbox from the page
+  */
 function deleteSearchBox(input_id) {
   let searchbox = document.getElementById(input_id+"_searchbox");
   if (searchbox != null) {
@@ -20,22 +36,19 @@ function deleteSearchBox(input_id) {
   }
 }
 
-// removes items from the list based on whether they equal a value
-function removeItemFromList(list, text) {
-    var items = list.getElementsByTagName("li");
-    for (var i = 0; i < items.length; ++i) {
-        if (items[i].getAttribute('data') === text) {
-            list.removeChild(items[i]);
-        }
-    }
-}
-
+/**
+  * creates a unordered list to contain the autocomplete items
+  */
 function createList(input_id) {
     var innerList = document.createElement('ul');
     innerList.setAttribute("id", input_id+"_searchlist");
     return innerList;
 }
 
+/**
+  * creates a div that sits under a given input, as a
+  * container to the autocomplete functionality
+  */
 function createSearchBox(input_id) {
     let searchbox = document.createElement('div');
     searchbox.setAttribute("id", input_id+"_searchbox");
@@ -43,12 +56,45 @@ function createSearchBox(input_id) {
     return searchbox;
 }
 
-function addAutoComplete(id, getdata) {
+function addDropdown(id, getdata) {
+    let element = document.getElementById(id);
+    let nameOfElement = element.getAttribute("name");
+    let parent = element.parentNode;
+    parent.removeChild(element);
+    let select = document.createElement('select');
+    select.setAttribute("id", id);
+    select.setAttribute("name", nameOfElement);
+    let option;
 
-    let list = createList(id);
+    getdata().forEach(function( item ) {
+        option = document.createElement('option');
+        option.value = option.textContent = item;
+        select.appendChild( option );
+    });
+    let defaultOption = document.createElement('option');
+    defaultOption.textContent = "Choose here";
+    defaultOption.setAttribute("selected", "");
+    defaultOption.setAttribute("disabled", "");
+    defaultOption.setAttribute("hidden", "");
+    select.appendChild(defaultOption)
+
+    parent.appendChild(select);
+}
+
+/**
+  * adds an autocomplete functionality to a given input.
+  * this allows a nicer user experience for the user - as they type,
+  * the possibilties are narrowed down, and they click on an item to 
+  * select it.
+  */
+function addAutoComplete(id, getdata) {
 
     let element = document.getElementById(id);
 
+    /**
+      * If the autocomplete searchbox is open and the user presses
+      * the escape key, remove the searchbox
+      */
     function considerRemovingSearchBoxOnPressingEscape(event) {
         if (event.key === "Escape") {
             deleteSearchBox(id);
@@ -77,7 +123,7 @@ function addAutoComplete(id, getdata) {
     document.addEventListener('click', considerKillingThisModalIfOutsideClick);
     element.addEventListener('blur', handleBlurEvent);
 
-    function considerAutoComplete(event) {
+    function openAutoComplete(event) {
         if (event.key === "Escape") return;
         // as the user presses keys, we keep checking what is current
         // and we provide a search box for that.
@@ -91,6 +137,8 @@ function addAutoComplete(id, getdata) {
         } else {
             // if there's more than one item in the input...
              
+           let list = createList(id);
+
             // if the searchbox doesn't exist, create it...
             if (document.getElementById(id+"_searchbox") == null) {
                 let searchbox = createSearchBox(id);
@@ -98,26 +146,59 @@ function addAutoComplete(id, getdata) {
                 searchbox.appendChild(list);
             }
 
-            // clear the list
-            list.innerHTML = '';
+            // get data for the list.
+            let mydata = getdata();
 
-            // add filtered data to the list
-            let filteredList = getdata().filter(text => text.includes(currentContent)).slice(0,5);
-            for (var i = 0; i < filteredList.length; i++) {
-                list.appendChild(createItemForList(filteredList[i], id))
-            }
-            
+            addFilteredData(list, mydata, currentContent, id);
+
         }
     }
 
     // if the user clicks, open the autocomplete
-    element.addEventListener('keyup', considerAutoComplete);
-    element.addEventListener('click', considerAutoComplete);
+    element.addEventListener('keyup', openAutoComplete);
+    element.addEventListener('click', openAutoComplete);
 
 }
 
+/**
+  * fills the list with data depending on what the user has typed into 
+  * the input field.  
+  * @param list the list in the searchbox which holds the results
+  * @param data the whole set of data from which to filter
+  * @param currentContent what the user has currently typed into the input field
+  * @param id the core identifier passed around.  other components build off that,
+  *        e.g. if the id is abc, then the list item might be 
+  */
+function addFilteredData(list, data, currentContent, id) {
+    let maxSearchListSize = 5;
+    let filteredList = 
+        data.filter(text => text.includes(currentContent))
+            .slice(0,maxSearchListSize);
+    for (var i = 0; i < filteredList.length; i++) {
+        let item = createItemForList(filteredList[i]);
+        // if the user clicks, put the value in the input field
+        addItemClickBehavior(item, id);
+        list.appendChild(item);
+    }
+    return list;
+}
 
+/**
+  * disables an input if there's no data for it.  I mean, there's 
+  * no point then, is there?
+  */
+function lockInput(id) {
+    let input = document.getElementById(id);
+    input.placeholder = "locked - no data";
+    input.disabled = true;
+}
 
+/**
+  * Communicates with the server.  
+  * @param verb - GET or POST
+  * @param path - the "action", the route to the endpoint.  e.g. "fib" or "math"
+  * @param data - if posting, the data to send to the server.
+  */ 
 function talk(verb, path, data) {
   return new Promise((resolve, reject) => {
     let r = new XMLHttpRequest();
@@ -136,33 +217,49 @@ function talk(verb, path, data) {
   });
 }
 
+/**
+  *  converts a json object to an array of strings
+  *  given a lambda (extractor) to use in the map function
+  *  to extract the string desired.  if parsing fails, do nothing.
+  */
 let extractData = function(value, extractor) {
   try {
     return JSON.parse(value).map(extractor).sort();
   } catch (error) {
-    console.log("unable to parse the following as JSON: " + value);
+    // do nothing
+    // console.log("unable to parse the following as JSON: " + value);
+  }
+}
+
+// Add an autocomplete to the book input for lending
+talk("GET", "book")
+.then(function(v){
+  betterUserExperienceForInput(v, book => book.Title, "lend_book");
+});
+
+// Add an autocomplete to the borrower input for lending
+talk("GET", "borrower")
+.then(function(v) {
+    betterUserExperienceForInput(v, borrower => borrower.Name, "lend_borrower");
+});
+
+function betterUserExperienceForInput(v, extractor, input_id) {
+  let receivedData = extractData(v, extractor);
+  if (receivedData == null) {
+    lockInput(input_id);
+  } else if (receivedData && receivedData.length > 0 && receivedData.length < 10) {
+    addDropdown(input_id, function() {return receivedData});
+  } else {
+    addAutoComplete(input_id, function() {return receivedData});
   }
 }
 
 
-talk("GET", "book")
-.then(function(v){
-  let bookdata = extractData(v, book => book.Title);
-  addAutoComplete("lend_book", function() {return bookdata});
-});
 
-talk("GET", "borrower")
-.then(function(v) {
-  let borrowerdata = extractData(v, borrower => borrower.Name);
-  addAutoComplete("lend_borrower", function() {return borrowerdata});
-});
+function integration_tests() {
+console.log("WARNING: integration tests running - these affect state");
 
-
-
-
-/*
-// this should clean the database
-
+// clean the database to begin
 talk("GET", "flyway")
 .then(function(v){
   return talk("POST", "registerborrower", "borrower=alice");
@@ -174,9 +271,39 @@ talk("GET", "flyway")
   console.assert(v === '[{"Name": "alice", "Id": "1"}]', 'result was ' + v);  
   return JSON.parse(v).map(borrower => borrower.Name);
 })
-.then(function(v) {console.assert(v[0] === "alice", "result should be alice, was " + v[0])});
+.then(function(v) {
+    console.assert(v[0] === "alice", "result was " + v[0])
+    return v;
+})
+.then(function(v) {
+    talk("GET", "flyway");
+});
+}
+// uncomment the function below to run integration tests
+// integration_tests();
 
-*/
+function unit_tests() {
+// testing the createItemForList basics
+let item = createItemForList("abc");
+console.assert(item.getAttribute("data") === "abc", "data attribute was " + item.getAttribute("data") );
+console.assert(item.innerText === "abc", "the inner text was " + item.innerText )
+
+// testing createList
+let list = createList("abc")
+console.assert(list.id === "abc_searchlist", "list id was " + list.id);
+
+// testing createSearchBox
+let searchbox = createSearchBox("abc");
+console.assert(searchbox.id === "abc_searchbox")
+
+// testing addFilteredData
+let mydata = ['a','b','c'];
+let currentContent = 'a';
+let filteredDataList = addFilteredData(list, mydata, currentContent, "abc");
+
+}
+// uncomment the function below to run unit tests
+// unit_tests();
 
 
    
