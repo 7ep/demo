880a542d103a311555514273a376f4b84e5ed7e1
Byron Katz <byronka@msn.com>
2020-04-18 14:13:24 -0400

    Removing loops and timing.  Whitespace refactors.

--- src/ui_tests/java/src/test/java/com/coveros/training/HtmlUnitTests.java
+++ src/ui_tests/java/src/test/java/com/coveros/training/HtmlUnitTests.java
@@ -43,9 +43,9 @@ public class HtmlUnitTests {
         }
     }
 
-    private void type (DomElement input, String text) {
+    private void type(DomElement input, String text) {
         try {
-            ((HtmlTextInput)input).type(text);
+            ((HtmlTextInput) input).type(text);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
@@ -53,41 +53,31 @@ public class HtmlUnitTests {
 
     /**
      * Tests the entire process of lending -
-     *  registers a book, a borrower, and lends it.
+     * registers a book, a borrower, and lends it.
      * See {{@link SeleniumTests#test_shouldLendBook} for a
      * full javascript version that runs on Chrome
      */
     @Test
     public void test_shouldLendBook() {
-        long start = System.currentTimeMillis();
+        HtmlPage page = getPage("http://localhost:8080/demo/library.html");
+        page = click(page.querySelector(".button-form:nth-child(4) > input"));
+        page = click(page.getAnchorByText("Return"));
+        page = click(page.getElementById("register_book"));
+        type(page.getElementById("register_book"), "some book");
+        page = click(page.getElementById("register_book_submit"));
+        page = click(page.getAnchorByText("Return"));
+        page = click(page.getElementById("register_borrower"));
+        type(page.getElementById("register_borrower"), "some borrower");
+        page = click(page.getElementById("register_borrower_submit"));
+        page = click(page.getAnchorByText("Return"));
+        page = click(page.getElementById("lend_book"));
+        type(page.getElementById("lend_book"), "some book");
+        type(page.getElementById("lend_borrower"), "some borrower");
+        page = click(page.getElementById("lend_book_submit"));
+        final DomElement result = page.getElementById("result");
 
-        for (int i = 0; i < 10; i++) {
-            HtmlPage page = getPage("http://localhost:8080/demo/library.html");
-            page = click(page.querySelector(".button-form:nth-child(4) > input"));
-            page = click(page.getAnchorByText("Return"));
-            page = click(page.getElementById("register_book"));
-            type(page.getElementById("register_book"), "some book");
-            page = click(page.getElementById("register_book_submit"));
-            page = click(page.getAnchorByText("Return"));
-            page = click(page.getElementById("register_borrower"));
-            type(page.getElementById("register_borrower"), "some borrower");
-            page = click(page.getElementById("register_borrower_submit"));
-            page = click(page.getAnchorByText("Return"));
-            page = click(page.getElementById("lend_book"));
-            type(page.getElementById("lend_book"), "some book");
-            type(page.getElementById("lend_borrower"), "some borrower");
-            page = click(page.getElementById("lend_book_submit"));
-            final DomElement result = page.getElementById("result");
-
-            assertEquals("SUCCESS", result.getTextContent());
-        }
-
-        long finish = System.currentTimeMillis();
-        long timeElapsed = finish - start;
-        System.out.println("Time elapsed: " + timeElapsed + " millseconds");
+        assertEquals("SUCCESS", result.getTextContent());
     }
-
-
 
 
 }
