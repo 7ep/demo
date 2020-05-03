cf731609b2f02998a966f0b234f7b1b48ec6d0f5
Byron Katz <byronka@msn.com>
2020-05-03 15:30:05 -0400

    Documenting servlet utils

--- src/main/java/com/coveros/training/helpers/ServletUtils.java
+++ src/main/java/com/coveros/training/helpers/ServletUtils.java
@@ -6,6 +6,11 @@ import org.slf4j.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * These helper methods help to remove duplication in
+ * the servlets.  See the methods for more detail on
+ * what they do.
+ */
 public class ServletUtils {
 
     public static final String RESTFUL_RESULT_JSP = "restfulresult.jsp";
@@ -15,6 +20,10 @@ public class ServletUtils {
         // using a private constructor to hide the implicit public one.
     }
 
+    /**
+     * A user posts information in the regular web application, and we react by forwarding them to another web page.
+     * See result.jsp to get an idea.
+     */
     public static void forwardToResult(HttpServletRequest request, HttpServletResponse response, Logger logger) {
         try {
             request.getRequestDispatcher(RESULT_JSP).forward(request, response);
@@ -23,6 +32,10 @@ public class ServletUtils {
         }
     }
 
+    /**
+     * A user has made a RESTful web API call, and we are responding with the minimal syntax.
+     * See restfulresult.jsp to get an idea.
+     */
     public static void forwardToRestfulResult(HttpServletRequest request, HttpServletResponse response, Logger logger) {
         try {
             request.getRequestDispatcher(RESTFUL_RESULT_JSP).forward(request, response);
