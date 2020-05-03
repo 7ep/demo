fd3e0ac47e965d35a0e3ac4bb031c7f3ea776b55
Byron Katz <byronka@msn.com>
2020-05-03 15:25:01 -0400

    Documenting enums for registration

--- src/main/java/com/coveros/training/authentication/domainobjects/PasswordResultEnums.java
+++ src/main/java/com/coveros/training/authentication/domainobjects/PasswordResultEnums.java
@@ -1,5 +1,9 @@
 package com.coveros.training.authentication.domainobjects;
 
+/**
+ * All the values currently possible for our
+ * assessment of a potential password
+ */
 public enum PasswordResultEnums {
     TOO_SHORT,
     TOO_LONG,  // ironic, I know.  But if it's too long we're open to DOS attacks.  It takes too long to process extremely long passwords.
--- src/main/java/com/coveros/training/authentication/domainobjects/RegistrationStatusEnums.java
+++ src/main/java/com/coveros/training/authentication/domainobjects/RegistrationStatusEnums.java
@@ -1,11 +1,15 @@
 package com.coveros.training.authentication.domainobjects;
 
+/**
+ * All the values possible for the results of an
+ * attempted registration
+ */
 public enum RegistrationStatusEnums {
     ALREADY_REGISTERED,
     EMPTY_USERNAME,
     EMPTY_PASSWORD,
     SUCCESSFULLY_REGISTERED,
     BAD_PASSWORD,
-    EMPTY // represents the state of no status.
+    EMPTY // represents the state of no status.  Used by the empty registration result.
 
 }
